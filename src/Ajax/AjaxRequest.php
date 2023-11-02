7f43e0bc486f11a54e49f1024b8c9a5b11bf1be3
MichaelBengtsson <michael@krokedil.se>
2023-11-02 17:24:27 +0100

    Add AjaxRegistry service to handle ajax requests registration and execution

---
+++ src/Ajax/AjaxRequest.php
@@ -0,0 +1,85 @@
+<?php
+namespace Krokedil\Shipping\Ajax;
+
+/**
+ * Class AjaxRequest
+ *
+ * Defines a single AJAX request and how to process it.
+ */
+class AjaxRequest {
+	/**
+	 * The name of the AJAX action.
+	 *
+	 * @var string
+	 */
+	private $action;
+
+	/**
+	 * The callback function to process the AJAX request.
+	 *
+	 * @var callable
+	 */
+	private $callback;
+
+	/**
+	 * If the AJAX request requires a logged in user.
+	 *
+	 * @var bool
+	 */
+	private $no_priv;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param string $action
+	 * @param callable $callback
+	 * @param bool $no_priv
+	 * @return void
+	 */
+	public function __construct( $action, $callback, $no_priv = false ) {
+		$this->action   = $action;
+		$this->callback = $callback;
+		$this->no_priv  = $no_priv;
+	}
+
+	/**
+	 * Get the name of the AJAX action.
+	 *
+	 * @return string
+	 */
+	public function get_action() {
+		return $this->action;
+	}
+
+	/**
+	 * Get the callback function to process the AJAX request.
+	 *
+	 * @return callable
+	 */
+	public function get_callback() {
+		return $this->callback;
+	}
+
+	/**
+	 * Get if the AJAX request requires a logged in user.
+	 *
+	 * @return bool
+	 */
+	public function get_no_priv() {
+		return $this->no_priv;
+	}
+
+	/**
+	 * Process the AJAX request.
+	 *
+	 * @return void
+	 */
+	public function process() {
+		// Check the nonce.
+		check_ajax_referer( $this->action, 'nonce' );
+
+		call_user_func( $this->callback );
+
+		wp_die();
+	}
+}
