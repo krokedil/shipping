23afe7bc19077feee0d11edca69c5222a8135dd5
MichaelBengtsson <michael@krokedil.se>
2023-11-30 14:55:50 +0100

    Update html-pickup-point-select.php

--- templates/html-pickup-point-select.php
+++ templates/html-pickup-point-select.php
@@ -13,7 +13,7 @@ if ( empty( $pickup_points ) ) {
 ?>
 <div class="krokedil_shipping_pickup_point form-row">
 	<select id="krokedil_shipping_pickup_point" name="krokedil_shipping_pickup_point"
-		class="krokedil_shipping_pickup_point__select" data-rate-id="<?php esc_attr_e( $rate_id ) ?>">
+		class="krokedil_shipping_pickup_point__select" data-rate-id="<?php esc_attr_e( $rate_id ); ?>">
 		<?php foreach ( $pickup_points as $pickup_point ) : ?>
 			<option class="krokedil_shipping_pickup_point__option"
 				value="<?php echo esc_attr( $pickup_point->get_id() ); ?>" <?php selected( $pickup_point->get_id(), $selected_pickup_point ? $selected_pickup_point->get_id() : $pickup_points[0]->get_id() ); ?>>
