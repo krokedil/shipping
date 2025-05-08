f97fa1289402309fd79fb49c1e8184cae4c7c357
MichaelBengtsson <michael@krokedil.se>
2025-05-08 17:00:56 +0200

    Add shipping rate and shipping rate output classes with interface for shipping rate service

---
+++ src/Frontend/ShippingRateOutput.php
@@ -0,0 +1,59 @@
+<?php
+namespace Krokedil\Shipping\Frontend;
+
+use Krokedil\Shipping\Interfaces\ShippingRateServiceInterface;
+
+/**
+ * Class ShippingRateOutput. Handles the output for extra shipping rate information on the checkout page.
+ */
+class ShippingRateOutput {
+	/**
+	 * The shipping rate service instance.
+	 *
+	 * @var ShippingRateServiceInterface
+	 */
+	private $shipping_rate_service;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param ShippingRateServiceInterface $shipping_rate_service The shipping rate service instance.
+	 */
+	public function __construct( $shipping_rate_service ) {
+		$this->shipping_rate_service = $shipping_rate_service;
+
+		add_action( 'woocommerce_after_shipping_rate', array( $this, 'render_description' ), 5 );
+	}
+
+	public function should_output( $element, $shipping_rate ) {
+		// Only if this is the selected shipping rate.
+		if ( ! in_array( $shipping_rate->id, WC()->session->get( 'chosen_shipping_methods' ), true ) ) {
+			return false;
+		}
+
+		return apply_filters( "krokedil_shipping_should_output_$element", $this->shipping_rate_service->should_output( $element ), $shipping_rate );
+	}
+
+	/**
+	 * Render the shipping rate description.
+	 *
+	 * @param \WC_Shipping_Rate $shipping_rate The shipping rate instance.
+	 *
+	 * @return void
+	 */
+	public function render_description( $shipping_rate ) {
+		// Only if this is the selected shipping rate.
+		if ( ! $this->should_output( 'description', $shipping_rate ) ) {
+			return;
+		}
+
+		// Get the description for the shipping rate.
+		$description = $this->shipping_rate_service->get_shipping_rate_description( $shipping_rate );
+
+		if ( ! empty( $description ) && 0 === did_action('krokedil_shipping_before_rate_description') ) {
+			do_action( 'krokedil_shipping_before_rate_description' );
+			echo '<div class="krokedil_shipping_description">' . wp_kses_post( $description ) . '</div>';
+			do_action( 'krokedil_shipping_after_rate_description' );
+		}
+	}
+}
---
+++ src/ShippingRate.php
@@ -0,0 +1,107 @@
+<?php
+namespace Krokedil\Shipping;
+
+use Krokedil\Shipping\Interfaces\ShippingRateServiceInterface;
+use Krokedil\Shipping\Container\Container;
+use Krokedil\Shipping\Traits\RateData;
+use Krokedil\Shipping\Frontend\ShippingRateOutput;
+
+/**
+ * Class to handle shipping rates and extensions to them for WooCommerce.
+ */
+class ShippingRate implements ShippingRateServiceInterface {
+	use RateData;
+
+	/**
+	 * The arguments for the class.
+	 *
+	 * @var array
+	 */
+	private $args = array(
+		'show_description' => true,
+	);
+
+	/**
+	 * The container instance.
+	 *
+	 * @var Container
+	 */
+	private $container;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Arguments to initialize the class with.
+	 *
+	 * @return void
+	 */
+	public function __construct( $args = array() ) {
+		$this->args = array_merge( $this->args, $args );
+		$this->init();
+	}
+
+	/**
+	 * Initialize the class instance.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		// Register actions and filters required by the package.
+		add_filter( 'woocommerce_hidden_order_itemmeta', array( $this, 'add_hidden_order_itemmeta' ) );
+
+		// Setup the container with the dependent services.
+		$this->container = Container::get_instance();
+		$this->container->add( 'session-handler', new SessionHandler() );
+		$this->container->add( 'shipping-rate-output', new ShippingRateOutput( $this ) );
+	}
+
+	/**
+	 * Add the order line metadata for pickup points to the list of hidden meta data.
+	 *
+	 * @param array $hidden_order_itemmeta The list of hidden meta data.
+	 * @return array
+	 */
+	public function add_hidden_order_itemmeta( $hidden_order_itemmeta ) {
+		$hidden_order_itemmeta[] = 'krokedil_description';
+
+		return $hidden_order_itemmeta;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_container() {
+		return $this->container;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_shipping_rate_description( $rate ) {
+		$meta_data = $rate->get_meta_data();
+		if ( ! array_key_exists( 'krokedil_description', $meta_data ) ) {
+			return '';
+		}
+
+		return $meta_data['krokedil_description'];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function save_shipping_rate_description( $rate, $description ) {
+		// If the description is empty, return.
+		if ( empty( $description ) ) {
+			return;
+		}
+
+		$this->save_shipping_rate_data( $rate, array( 'krokedil_description' => $description ) );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function should_output( $element ) {
+		return $this->args[ "show_{$element}" ];
+	}
+}
