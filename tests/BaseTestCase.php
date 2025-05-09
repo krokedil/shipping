33e1d8d8db8acc5eb93f784127fa4b89114d535c
MichaelBengtsson <michael@krokedil.se>
2025-05-09 13:38:59 +0200

    Update BaseTestCase.php

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -1,5 +1,7 @@
 <?php
-use Krokedil\Shipping\Container;
+use Krokedil\Shipping\AJAX;
+use Krokedil\Shipping\Assets;
+use Krokedil\Shipping\Container\Container;
 use Krokedil\Shipping\Interfaces\PickupPointServiceInterface;
 use WP_Mock\Tools\TestCase;
 
@@ -9,6 +11,7 @@ abstract class BaseTestCase extends TestCase {
 	protected $mockShipping;
 	protected $mockSession;
 	protected $mockCart;
+	protected $mockCountries;
 
 	// Internal mocks.
 	protected $mockPickupPointService;
@@ -58,10 +61,12 @@ abstract class BaseTestCase extends TestCase {
 		$this->mockShipping    = Mockery::mock( 'WC_Shipping' );
 		$this->mockSession     = Mockery::mock( 'WC_Session' );
 		$this->mockCart        = Mockery::mock( 'WC_Cart' );
+		$this->mockCountries   = Mockery::mock( 'WC_Countries' );
 
 		$this->mockWoocommerce->shouldReceive( 'shipping' )->andReturn( $this->mockShipping );
-		$this->mockWoocommerce->session = $this->mockSession;
-		$this->mockWoocommerce->cart    = $this->mockCart;
+		$this->mockWoocommerce->session   = $this->mockSession;
+		$this->mockWoocommerce->cart      = $this->mockCart;
+		$this->mockWoocommerce->countries = $this->mockCountries;
 
 		WP_Mock::userFunction( 'WC' )->andReturn( $this->mockWoocommerce );
 	}
