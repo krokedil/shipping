a76edcb6ebc70639cf73144fcf578cb4af7cdbf9
MichaelBengtsson <michael@krokedil.se>
2025-05-09 13:45:14 +0200

    Update PickupPoints.php

--- src/PickupPoints.php
+++ src/PickupPoints.php
@@ -259,39 +259,6 @@ class PickupPoints implements PickupPointServiceInterface {
 	}
 
 	/**
-	 * Ensure the saving of the pickup point data to the shipping rate. Either by forcing the session to update or by updating the shipping rate directly.
-	 *
-	 * @param \WC_Shipping_Rate    $rate The shipping rate we want to update.
-	 * @param array<string, mixed> $data The data to update the rate id with. The key is the meta data id and the value is the data to save.
-	 *
-	 * @return void
-	 */
-	private function save_shipping_rate_data( $rate, $data ) {
-		$can_set_meta = ( doing_action( 'woocommerce_before_get_rates_for_package' ) || doing_action( 'woocommerce_after_get_rates_for_package' ) || doing_filter( 'woocommerce_package_rates' ) );
-
-		// If before calculate_totals has been triggered but not after, we can just update the meta data for the shipping rate.
-		if ( $can_set_meta ) {
-			foreach ( $data as $key => $value ) {
-				$rate->add_meta_data( $key, $value );
-			}
-		} else {
-			// Otherwise we want to force the shipping rates to update.
-			/**
-			 * The session handler service from the pickup point service container.
-			 *
-			 *  @var SessionHandler $session_handler
-			 */
-			$session_handler = $this->container->get( 'session-handler' );
-
-			// Save the data to the session handler.
-			$session_handler->set_shipping_rate_data( $rate->get_id(), $data );
-
-			// Force the shipping rates to update.
-			$session_handler->update_shipping_rates();
-		}
-	}
-
-	/**
 	 * Return any pickup point shipping methods from a WooCommerce order.
 	 *
 	 * @param \WC_Order $order The WooCommerce order.
